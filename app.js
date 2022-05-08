7b2841295ed9ac3f718c275f5bfafa1e7d267493
QuickScope-san <kevin.cancimanse@correounivalle.edu.co>
2022-05-08 18:53:03 -0500

    MERN Stack

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const operation = require('./resources/operations');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/pares', (req, res) => {
+  const {numero} = req.body;
+  res.send(operation.pares(numero));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
