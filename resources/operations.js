3f0ddfc91cee40d3f5c4f9301228f96c6bcaaa11
QuickScope-san <kevin.cancimanse@correounivalle.edu.co>
2022-05-10 21:59:43 -0500

    Feature added

--- resources/operations.js
+++ resources/operations.js
@@ -17,6 +17,32 @@ let pares = (x) => {
     return resp;
  }
 
+let raiz_impar = (x) => {
+
+   //resp = "Los numeros son :"
+   let resp = "los numeros impares entero(s) de la raíz cuadrada del numero en cuestion son:";
+
+   for (let i = 1; i <= x; i++) {
+    
+    let n = Math.sqrt(i)
+
+    if( (n % 2 != 0) && (n % 1 === 0 )){
+
+        resp = resp + ` ${n}`;
+
+    }
+
+ }
+
+    return resp;
+
+}
+
+//let n = 16;
+//answer = raiz_impar(n);
+//console.log("odd numbers are: " + answer);
+
 module.exports={
     pares,
+    raiz_impar,
 }
